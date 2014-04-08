c7b35d0df1299ce6bcc7144751d9390d9d77a1bd
Edgar M. Madrid <m.madrid16@gmail.com>
2014-04-07 22:53:57 -0300

    virtual env and static files

--- swingdominicano/urls.py
+++ swingdominicano/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+import settings 
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,6 +12,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'swingdominicano.views.home', name='home'),
     url(r'^$', 'app.views.index', name='index'),
     url(r'^post$', 'app.views.post', name='post'),
+    url(r'^post$', 'contact.views.contact', name='contact'),
     # url(r'^swingdominicano/', include('swingdominicano.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -17,3 +21,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #patterns('',
+        #static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+       #(r'^/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    #)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mmadrid/Development/work/swingdominicano/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
