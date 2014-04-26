4830c0ab56e01cc0c155ce88d4253e31828e1ec9
Edgar M. Madrid <m.madrid16@gmail.com>
2014-04-25 23:13:20 -0300

    video view and relation with sqlite

--- swingdominicano/settings.py
+++ swingdominicano/settings.py
@@ -138,6 +138,8 @@ INSTALLED_APPS = (
     'photos',
     'video',
     'tinymce',
+    'data_migration',
+    'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- video/views.py
+++ video/views.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
+from models import Video
+
 
 def video(request):
-	return render_to_response('video.html',locals())
+    video_principal = Video.objects.filter(active=True).values()[0]
+    videos = Video.objects.all().exclude(id=video_principal['id']).values()
+    return render_to_response('video.html', locals())
